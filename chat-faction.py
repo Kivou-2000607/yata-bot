9fca3ac8d4164e3d87fbb6a2a88a3935882b0661
Kivou [2000607] <kivou.2000607@gmail.com>
2020-04-02 15:32:40 +0200

    chat faction @leader

--- chat-faction.py
+++ chat-faction.py
@@ -1,36 +1,43 @@
 from discord import Webhook, AsyncWebhookAdapter
 
+import re
 import asyncio
 import websockets
 import cloudscraper
 import aiohttp
 import json
-import re
 
 from includes.yata_db import get_secret
 import includes.formating as fmt
 
 room = "Faction:33241"
-iud, secret, hookurl = get_secret(room)
+iud, secret, hooks = get_secret(room)
 
 
-async def chat(uid, secret, hookurl, room):
+async def chat(uid, secret, hooks, room):
 
     uri = f"wss://ws-chat.torn.com/chat/ws?uid={iud}&secret={secret}"
 
     token, agent = cloudscraper.get_cookie_string("https://www.torn.com")
     headers = {"User-Agent": agent, "Cookie": token}
 
+
     async with websockets.connect(uri, origin="https://www.torn.com", extra_headers=headers) as websocket:
         async with aiohttp.ClientSession() as session:
-            webhook = Webhook.from_url(hookurl, adapter=AsyncWebhookAdapter(session))
+            webhooks = dict({})
+            for hookId, hookurl in hooks.items():  
+                webhooks[hookId] = Webhook.from_url(hookurl, adapter=AsyncWebhookAdapter(session))
+                
             while(True):
                 data = await websocket.recv()
                 d = json.loads(data).get("data", [dict({})])[0]
                 if d.get("roomId", "") == room and d.get("messageText"):
                     msg = fmt.chat_message(d)
-                    if re.search("\W*(leader)\W*", msg.lower()) is not None:
-                        msg += " <@&546769358744191006>"
-                    await webhook.send(msg)
+                    await webhooks["full"].send(msg)
 
-asyncio.get_event_loop().run_until_complete(chat(iud, secret, hookurl, room))
+                    for keyword in [k for k in webhooks if k != "full"]:
+                        if re.search(f"\W*({keyword})\W*", msg.lower()) is not None:
+                            #msg += " <@&546769358744191006>"
+                            await webhooks[keyword].send(msg)
+
+asyncio.get_event_loop().run_until_complete(chat(iud, secret, json.loads(hooks), room))
