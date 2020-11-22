1d3549c5d40bee92e5babffcc6789925c6149449
Kivou [2000607] <kivou.2000607@gmail.com>
2020-11-22 22:28:58 +0100

    Set back notifications to bot 3

--- cogs/api.py
+++ cogs/api.py
@@ -42,7 +42,7 @@ from inc.handy import *
 class API(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        if self.bot.bot_id == 5:
+        if self.bot.bot_id == 3:
             self.notify.start()
 
     def cog_unload(self):
