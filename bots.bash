33031036f9f3ed128fff2a789f677ea633367154
Kivou [2000607] <kivou.2000607@gmail.com>
2021-01-06 14:55:12 +0100

    minor

--- bots.bash
+++ bots.bash
@@ -5,25 +5,33 @@ source /usr/local/share/venv_py3.9/bin/activate
 #pip install -U cloudscraper
 
 # move to appropriate folder
-cd ~/yata-bot/
+# cd ~/yata-bot/
 
 # run the yata bot
+echo "    kill yata"
 kill -9 $(cat pids/yata.pid)
+echo "    run yata"
 nohup python yata.py ".env-yata" > logs/yata.log 2>&1 &
 echo $! > pids/yata.pid
 
 # run marvin
+echo "    kill marvin"
 kill -9 $(cat pids/marvin.pid)
+echo "    run marvin"
 nohup python yata.py ".env-marvin" > logs/marvin.log 2>&1 &
 echo $! > pids/marvin.pid
 
 # run the nub bot
+echo "    kill nub bot"
 kill -9 $(cat pids/nub.pid)
+echo "    run nub bot"
 nohup python yata.py ".env-nub" > logs/nub.log 2>&1 &
 echo $! > pids/nub.pid
 
 # send the logs
+echo "    kill logs"
 kill -9 $(cat pids/logs.pid)
+echo "    run logs"
 nohup python logs.py ".env-yata" > logs/logs.log 2>&1 &
 echo $! > pids/logs.pid
 
