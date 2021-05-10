3c0a3d4845b0c14e61f053c583d5db258484167e
Kivou-2000607 <kivou.2000607@gmail.com>
2021-05-10 22:26:47 +0200

    snake

--- cogs/misc.py
+++ cogs/misc.py
@@ -36,6 +36,15 @@ class Misc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        # snake emoji variables
+        self.snake_e_u = "⬆️"
+        self.snake_e_d = "⬇️"
+        self.snake_e_l = "⬅️"
+        self.snake_e_r = "➡️"
+
+        # snake current games
+        self.snakes = {}
+
     @commands.command()
     @commands.bot_has_permissions(send_messages=True)
     @commands.guild_only()
@@ -59,3 +68,185 @@ class Misc(commands.Cog):
     async def crimes2(self, ctx):
         """gives latest update on crimes 2.0"""
         await ctx.send("https://yata.yt/media/misc/crimes2.gif")
+
+
+    @commands.command(aliases=["s"])
+    @commands.bot_has_permissions(send_messages=True)
+    @commands.guild_only()
+    async def snake(self, ctx):
+        """play snake"""
+
+        # game setup
+        size_x = 9
+        size_y = 11
+
+        # screen
+        s_s = ""
+        s_u = ":arrow_up_small:"
+        s_d = ":arrow_down_small:"
+        s_r = ":arrow_forward:"
+        s_l = ":arrow_backward:"
+        s_b = ":blue_square:"
+        s_v = ":black_large_square:"
+        s_a = ":green_square:"
+        s_e = ""
+        # s_s = "```\n"
+        # s_u = ":arrow_up_small:"
+        # s_d = ":arrow_down_small:"
+        # s_r = ":arrow_forward:"
+        # s_l = ":arrow_backward:"
+        # s_b = ":blue_square:"
+        # s_v = ":black_large_square:"
+        # s_a = ":green_square:"
+        # s_e = "```"
+
+        # helper function to pass from cartesian to lexicographical notations
+        def c2l(x, y):
+            return y * size_x + x
+
+        # helper function to pass from lexicographical to cartesian notations
+        def l2c(n):
+            return (n % size_x, n // size_x)
+
+        def spawn_apple(snake):
+            free = [_ for _ in range(size_x * size_y)]
+            for s in snake["body"]:
+                free.pop(s)
+
+            for a in snake["apples"]:
+                free.pop(a)
+
+            snake["apples"].append(random.choice(free))
+            return snake
+
+
+        def screen(snake):
+
+            # check if lost
+            if snake["lost"]:
+                return f'You lost... Snake size: {len(snake["body"])}'
+
+            screen = s_s
+            head = l2c(snake["body"][-1])
+            screen = f"{head}\n{s_s}"
+            for i in range(size_x * size_y):
+                if i == snake["body"][-1]:
+                    screen += snake["direction"]
+                elif i in snake["body"]:
+                    screen += s_b
+                elif i in snake["apples"]:
+                    screen += s_a
+                else:
+                    screen += s_v
+
+                if not (i + 1) % size_x:
+                    screen += "\n"
+
+            screen += s_s
+
+            return screen
+
+        def move(snake, direction=None):
+
+            # set same direction as current
+            if direction is None:
+                direction = snake["direction"]
+
+            # ignore invalid move
+            if sorted([snake["direction"], direction]) in [sorted([s_d, s_u]), sorted([s_r, s_l])]:
+                return snake
+
+            # record direction
+            snake["direction"] = direction
+
+            # add head
+            x, y = l2c(snake["body"][-1])
+            if direction == s_r:
+                x += 1
+            elif direction == s_l:
+                x -= 1
+            elif direction == s_d:
+                y += 1
+            elif direction == s_u:
+                y -= 1
+
+            # check if wall
+            if x == size_x or x == -1 or y == size_y or y == -1:
+                snake["lost"] = True
+                return snake
+
+            # lexico head
+            head = c2l(x, y)
+
+            # hit itself
+            if head in snake["body"]:
+                snake["lost"] = True
+                return snake
+
+
+            # check if apple
+            if head in snake["apples"]:
+                snake["apples"].remove(head)
+                snake = spawn_apple(snake)
+            else:
+                snake["body"].pop(0)
+
+            snake["body"].append(head)
+
+            return snake
+
+        # variables
+        snake = { "body": [c2l(size_x // 2 - 1, size_y // 2), c2l(size_x // 2, size_y // 2)],
+                  "direction": s_r, "lost": False, "apples": [c2l(size_x // 2 + 2, size_y // 2)] }
+
+        message = await ctx.send(screen(snake))
+        await message.add_reaction(self.snake_e_l)
+        await message.add_reaction(self.snake_e_u)
+        await message.add_reaction(self.snake_e_d)
+        await message.add_reaction(self.snake_e_r)
+        self.snakes[message.id] = snake
+
+        while True:
+            snake = self.snakes[message.id]
+            snake = move(snake)
+            await message.edit(content=screen(snake))
+            if snake["lost"]:
+                return
+
+            if len(snake["body"]) > 20:
+                pass
+            elif len(snake["body"]) > 10:
+                await asyncio.sleep(1)
+            elif len(snake["body"]) > 5:
+                await asyncio.sleep(2)
+            else:
+                await asyncio.sleep(3)
+
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, discord_user):
+        if discord_user.bot:
+            return
+
+        s_s = ""
+        s_u = ":arrow_up_small:"
+        s_d = ":arrow_down_small:"
+        s_r = ":arrow_forward:"
+        s_l = ":arrow_backward:"
+        s_b = ":blue_square:"
+        s_v = ":black_large_square:"
+        s_a = ":green_square:"
+        s_e = ""
+
+        if reaction.message.id in self.snakes:
+            snake = self.snakes[reaction.message.id]
+            if f"{reaction}" == self.snake_e_u:
+                snake["direction"] = s_u
+            elif f"{reaction}" == self.snake_e_d:
+                snake["direction"] = s_d
+            elif f"{reaction}" == self.snake_e_l:
+                snake["direction"] = s_l
+            elif f"{reaction}" == self.snake_e_r:
+                snake["direction"] = s_r
+
+            await reaction.message.remove_reaction(reaction, discord_user)
