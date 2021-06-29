eb490229d54fae12f856c2a9aae8a22c30e7dc20
Kivou-2000607 <kivou.2000607@gmail.com>
2021-06-30 00:09:42 +0200

    help

--- cogs/jfk.py
+++ cogs/jfk.py
@@ -37,6 +37,54 @@ class JFK(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(aliases=["n"])
+    @commands.bot_has_permissions(send_messages=True)
+    @commands.guild_only()
+    async def stalk(self, ctx):
+        """self assign command for jfk server"""
+
+        if not len(args):
+            await self.bot.send_error_message(ctx.channel, f"You need to give an argument like `!notify loot`", title="Self assign roles")
+            return
+
+        stocks_channels = [856913052201254952, 627980378996604938]
+        war_channels = [356171252706181140, 356143232435879937]
+        jfk_channels = [356137641814786053, 665592053308063767]
+        loot_channels = [627980403583746098, 653339671392419881]
+
+        arg_key = args[0][:3].lower()
+
+        if arg_key in ["loo", "npc"] and ctx.channel.id in loot_channels:
+            role = get(ctx.guild.roles, id=628403353428295681)
+        elif arg_key in ["sto"] and ctx.channel.id in jfk_channels + stocks_channels:
+            role = get(ctx.guild.roles, id=628403308192596038)
+        elif arg_key in ["inv"] and ctx.channel.id in jfk_channels + stocks_channels:
+            role = get(ctx.guild.roles, id=856913807631843329)
+        elif arg_key in ["ret"] and ctx.channel.id in war_channels:
+            ri = { 356171252706181140: 841431434572857354, 356143232435879937: 841431662318452786 }
+            role = get(ctx.guild.roles, id=ri[ctx.channel.id])
+        elif arg_key in ["chai"] and ctx.channel.id in war_channels:
+            ri = { 356171252706181140: 629005125234589707, 356143232435879937: 629005185880162355 }
+            role = get(ctx.guild.roles, id=ri[ctx.channel.id])
+
+
+
+        # if role is None
+        if role is None:
+            await self.bot.send_error_message(ctx.channel, f"No roles found for {args[0]}", title="Self assign roles")
+            return
+
+        if role in ctx.author.roles:
+            await ctx.author.remove_roles(role)
+            action = "removed"
+        else:
+            await ctx.author.add_roles(role)
+            action = "assigned"
+        embed = Embed(description=f"Role {role.mention} {action}.")
+        embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+        await send(ctx.channel, embed=embed)
+
+
 
     @commands.command()
     @commands.bot_has_permissions(send_messages=True)
@@ -76,10 +124,6 @@ class JFK(commands.Cog):
             await ctx.author.add_roles(role)
             msg = await ctx.send(f'```role @{role.name} added to {ctx.author.display_name}```')
 
-        # await asyncio.sleep(5)
-        # await msg.delete()
-        # await ctx.message.delete()
-
     @commands.command()
     @commands.bot_has_permissions(send_messages=True)
     @commands.guild_only()
@@ -110,10 +154,6 @@ class JFK(commands.Cog):
         else:
             await ctx.author.add_roles(role)
             msg = await ctx.send(f'```role @{role.name} added to {ctx.author.display_name}```')
-
-        # await asyncio.sleep(5)
-        # await msg.delete()
-        # await ctx.message.delete()
 
     @commands.command()
     @commands.bot_has_permissions(send_messages=True)
@@ -153,10 +193,6 @@ class JFK(commands.Cog):
             await ctx.author.add_roles(role)
             msg = await ctx.send(f'```role @{role.name} added to {ctx.author.display_name}```')
 
-        # await asyncio.sleep(5)
-        # await msg.delete()
-        # await ctx.message.delete()
-
 
     @commands.command()
     @commands.bot_has_permissions(send_messages=True)
@@ -195,7 +231,3 @@ class JFK(commands.Cog):
         else:
             await ctx.author.add_roles(role)
             msg = await ctx.send(f'```role @{role.name} added to {ctx.author.display_name}```')
-
-        # await asyncio.sleep(5)
-        # await msg.delete()
-        # await ctx.message.delete()
