e3bb2e7382fe05d425eab6dbe7cb95dfac4580e7
Kivou-2000607 <kivou.2000607@gmail.com>
2022-10-09 20:54:54 +0000

    systemctl

--- kill.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "    kill $1"
-kill -9 $(cat pids/$1.pid)
--- start.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ~/.virtualenvs/yata-bot/bin/activate
-
-echo "    kill $1"
-kill -9 $(cat pids/$1.pid)
-echo "    run $1"
-nohup python yata.py ".env-$1" > logs/$1.log 2>&1 &
-echo $! > pids/$1.pid
